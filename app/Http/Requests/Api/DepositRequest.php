4b99898b475f530a1880b88b9d7a4acb86026d8b
sophia <thangneimawi@aceplussolutions.com>
2024-10-06 12:34:11 +0700

    fixed payment

--- app/Http/Requests/Api/DepositRequest.php
+++ app/Http/Requests/Api/DepositRequest.php
@@ -22,7 +22,7 @@ class DepositRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'agent_payment_type_id' => ['required', 'exists:payment_types,id'],
+            'agent_payment_type_id' => ['required', 'exists:banks,id'],
             'amount' => ['required', 'integer', 'min:1000'],
             'image' => ['required', 'file', 'image', 'mimes:jpg,png,jpeg', 'max:2048'],
             'refrence_no' => ['required', 'digits:6'],
