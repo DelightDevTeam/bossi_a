592428c3c646a9b694e25d03ea13ad54ed5fa3ba
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-25 15:09:16 +0630

    sub updated

--- resources/views/admin/agent/sub_agent_create.blade.php
+++ resources/views/admin/agent/sub_agent_create.blade.php
@@ -462,7 +462,7 @@
                 title: errorMessage,
                 background: 'hsl(230, 40%, 10%)',
                 showConfirmButton: false,
-                timer: 1500
+                timer: 150000
             })
         @endif
         function copy() {
